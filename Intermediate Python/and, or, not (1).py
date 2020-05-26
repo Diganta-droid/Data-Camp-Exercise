24b86258b898d9bebcd5a97981d196e157a71260
Dignata Majumder <57372132+Diganta-droid@users.noreply.github.com>
2020-05-26 15:37:00 +0600

    Update and, or, not (1).py

--- Intermediate Python/and, or, not (1).py
+++ Intermediate Python/and, or, not (1).py
@@ -21,4 +21,4 @@ print(my_kitchen>10 and my_kitchen<18)
 print(my_kitchen>14 or my_kitchen<17)
 
 # Double my_kitchen smaller than triple your_kitchen?
-print((my_kitchen**2)<(your_kitchen**3))
+print((my_kitchen*2)<(your_kitchen*3))
